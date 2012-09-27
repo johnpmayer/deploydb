e7e83b4e456e2dfeb319ed8474a07f9df8103c72
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2012-09-27 10:53:05 -0400

    removed old columns

--- host.sql
+++ host.sql
@@ -1,11 +1,9 @@
 create table deployapp.host (
 	id int not null generated always as identity
 ,	hw_address varchar(32) not null
-,	ip_address int
 ,	profile_id int
 ,	last_reported_ip int
 ,	deploy_stage int not null
 ,	primary key (id)
 ,	foreign key (profile_id) references deployapp.profile
-,	check (ip_address between 3 and 99)
 );
